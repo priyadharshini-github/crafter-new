0b7bb5a0a337c7ba7cdce0bfd0aed48952c61bed
admin admin <evaladmin@example.com>
2020-02-10 12:13:21 +0000

    Delete file /scripts/components/header.groovy

--- scripts/components/header.groovy
+++
@@ -1,8 +0,0 @@
-import org.craftercms.sites.editorial.SearchHelper
-import org.craftercms.sites.editorial.ProfileUtils
-
-def segment = ProfileUtils.getSegment(profile, siteItemService)
-def searchHelper = new SearchHelper(searchService, urlTransformationService)
-def articles = searchHelper.searchArticles(false, null, segment, 0, 2)
-
-templateModel.header = header
