e60b50df98a9180ab1b28fa59c41fccdc92c5da5
admin admin <evaladmin@example.com>
2020-02-14 12:46:56 +0000

    User admin wrote content /scripts/pages/template.groovy

--- scripts/pages/template.groovy
+++ scripts/pages/template.groovy
@@ -35,4 +35,6 @@ def jo = xmlJSONObj.toString(textIndent)
 def ParsingResult = new XmlSlurper().parseText(returnMessage)
 def categories = ParsingResult.'**'.findAll { node -> node.name() == 'catName' }*.text()
 
+templateModel.merchCategories = categories
+
 return categories
