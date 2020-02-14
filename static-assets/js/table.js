b31402426902b9861cb2b9e3586b9819e7c0d3e0
admin admin <evaladmin@example.com>
2020-02-14 12:12:09 +0000

    User admin wrote content /static-assets/js/table.js

--- static-assets/js/table.js
+++ static-assets/js/table.js
@@ -1,3 +1,8 @@
-$(document).ready(function() {
-    $('#example').DataTable();
-} );
+$(document).ready(function(){
+  $("#myInput").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#myTable tr").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+});
