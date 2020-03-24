eeacfb755b5c00f7aecb50526e6604505ead4d60
admin admin <evaladmin@example.com>
2020-03-24 08:54:25 +0000

    User admin wrote content /static-assets/js/table.js

--- static-assets/js/table.js
+++ static-assets/js/table.js
@@ -9,7 +9,18 @@ $(document).ready(function(){
     	window.location.href = "https://click.linksynergy.com/fs-bin/click?id=O9/jWAhJi3I&offerid=692069.5&type=3&subid=0";
     });
     $('a.display-block').click(function () {
-
+        $.ajax({
+		type: "POST",
+		url: "https://api.rakutenmarketing.com/coupon/1.0?mid=44543",
+		headers: {
+			"Authorization": "Bearer a5865b14e1b4ecd5944921cc88b8ff7"
+		},
+		success: function (res) {
+			console.log("data", res);
+		}
+	}).done(function (data) {
+	});
+	
     });
 });
 
