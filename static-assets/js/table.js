fecda902acb048e237955fae0ef9f45d59c22593
admin admin <evaladmin@example.com>
2020-03-24 09:27:50 +0000

    User admin wrote content /static-assets/js/table.js

--- static-assets/js/table.js
+++ static-assets/js/table.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
       $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
     });
   });
-  $('link').click(function () {
+  $('link').on("click", function() {
     	window.location.href = "https://click.linksynergy.com/fs-bin/click?id=O9/jWAhJi3I&offerid=692069.5&type=3&subid=0";
     });
     $('a.display-block').click(function () {
