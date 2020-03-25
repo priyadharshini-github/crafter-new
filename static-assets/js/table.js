55a8a7838b2f73a1ec534ac62e0b96dbb3ea12a6
admin admin <evaladmin@example.com>
2020-03-25 07:29:14 +0000

    User admin wrote content /static-assets/js/table.js

--- static-assets/js/table.js
+++ static-assets/js/table.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    userClickedStoreUrl();
   $("#myInput").on("keyup", function() {
     var value = $(this).val().toLowerCase();
     $("#myTable tr").filter(function() {
