b14bc8fa816238f50bb6f694b32fa3a4dc95076c
admin admin <evaladmin@example.com>
2020-03-26 09:15:23 +0000

    User admin wrote content /static-assets/js/table.js

--- static-assets/js/table.js
+++ static-assets/js/table.js
@@ -5,39 +5,6 @@ $(document).ready(function(){
       $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
     });
   });
-   $('a.display-block').click(function () {
-        $.ajax({
-    		type: "POST",
-    		url: "https://api.rakutenmarketing.com/coupon/1.0?mid=44543",
-    		headers: {
-    			"Authorization": "Bearer a5865b14e1b4ecd5944921cc88b8ff7"
-    		},
-    		success: function (res) {
-    			console.log("data", res);
-    		},
-    		error: function(errorThrown){
-             alert(errorThrown);
-          }
-    	});
-	
-    });
-    
-    $('a.code-css').click(function () {
-        $.ajax({
-    		type: "POST",
-    		url: "https://api.rakutenmarketing.com/coupon/1.0?mid=44543",
-    		headers: {
-    			"Authorization": "Bearer a5865b14e1b4ecd5944921cc88b8ff7"
-    		},
-    		success: function (res) {
-    			console.log("data", res);
-    		},
-    		error: function(errorThrown){
-             alert(errorThrown);
-          }
-    	});
-	
-    });
 });
 
 
