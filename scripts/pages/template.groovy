5959330b471b61d2b56d843ae4f850e67df091d4
admin admin <evaladmin@example.com>
2020-03-27 09:27:58 +0000

    User admin wrote content /scripts/pages/template.groovy

--- scripts/pages/template.groovy
+++ scripts/pages/template.groovy
@@ -46,7 +46,7 @@ def jo = xmlJSONObj.toString(textIndent)
 // def ParsingResult = new XmlSlurper().parseText(returnMessage)
 // def categories = ParsingResult.'**'.findAll { node -> node.name() == 'catName' }*.text()
 
-templateModel.merchCategories = Eval.me(categories);
+templateModel.merchCategories = categories;
 templateModel.status = status;
 
 return categories
