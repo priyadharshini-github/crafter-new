29ffb3fe42635a1694d94384ab8698eb5f72c44e
admin admin <evaladmin@example.com>
2020-03-27 13:54:44 +0000

    User admin wrote content /static-assets/js/useraction.js

--- static-assets/js/useraction.js
+++ static-assets/js/useraction.js
@@ -2,22 +2,21 @@ $(document).ready(function(){
 	$('div.off').click(function () {
         	$.ajax({
     			type: "POST",
-    			url: "http://vendor-api.eba-adup9t5c.us-east-2.elasticbeanstalk.com/api/vendor/user/action/736",
+    			url: "http://vendor-api.eba-adup9t5c.us-east-2.elasticbeanstalk.com/api/vendor/user/action/23456",
     			headers: {
     				"Authorization": "Basic dXNlcjE6cGFzc3dvcmQx",
     				"Accept": "application/json"
     			},
     			crossDomain: true,
     			data: {
-                     "MerchantID"	: "23456",
-                     "MerchantName" : "iCheapFlight",
-                     "VendorID" : "1",
-                     "UserAction": "Code copied",
-                     "OfferID": "124",
-                     "OfferDescription": "20% Discount upto 20$",
-                     "CouponCode": "DTRIP20",
-                     "UserActionID": "1"
-                    
+                     "memberId"	: "23456",
+                     "merchantId" : 1,
+                     "merchantName" : "iCheapFlight",
+                     "vendorId" : "1",
+                     "userAction": "Code copied",
+                     "offerId": "124",
+                     "offerDescription": "20% Discount upto 20$",
+                     "couponCode": "DTRIP20"
     			},
     			dataType: 'json',
     			success: function (res) {
