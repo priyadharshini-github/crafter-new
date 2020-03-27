a061eee0c8d999a9ae812232dad0f4fac9a7abd6
admin admin <evaladmin@example.com>
2020-03-27 05:05:16 +0000

    User admin wrote content /static-assets/js/useraction.js

--- static-assets/js/useraction.js
+++ static-assets/js/useraction.js
@@ -16,20 +16,7 @@ $(document).ready(function(){
 	
     	});
     
-    	$('a.code-css').click(function () {
-        	$.ajax({
-    			type: "POST",
-    			url: "https://api.rakutenmarketing.com/coupon/1.0?mid=44543",
-    			headers: {
-    				"Authorization": "Bearer a5865b14e1b4ecd5944921cc88b8ff7"
-    			},
-    			success: function (res) {
-    				console.log("data", res);
-    			},
-    			error: function(errorThrown){
-             			alert(errorThrown);
-          		}
-    		});
-	
+    	$('#link1').click(function () {
+ 			alert("Code copied");
     	});
 });
