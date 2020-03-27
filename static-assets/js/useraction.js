3a4fabb4ad542cf8b3da378fb6297e17c8fff534
admin admin <evaladmin@example.com>
2020-03-27 06:11:48 +0000

    User admin wrote content /static-assets/js/useraction.js

--- static-assets/js/useraction.js
+++ static-assets/js/useraction.js
@@ -17,24 +17,8 @@ $(document).ready(function(){
     	});
     
     	$('div.code').click(function () {
- 		   toastr.options = {
-          "positionClass" : "toast-center-center",
-          "closeButton" : false,
-          "debug" : false,
-          "newestOnTop" : false,
-          "progressBar" : false,
-          "preventDuplicates" : false,
-          "onclick" : null,
-          "showDuration" : "300",
-          "hideDuration" : "1000",
-          "timeOut" : "5000",
-          "extendedTimeOut" : "1000",
-          "showEasing" : "swing",
-          "hideEasing" : "linear",
-          "showMethod" : "fadeIn",
-          "hideMethod" : "fadeOut"
-         }
-   Command: toastr["success"]
-           ("Code Copied!")
+ 		  var x = document.getElementById("toast");
+  x.className = "show";
+  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
     	});
 });
