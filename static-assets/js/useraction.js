ad316f610aec10077cf031a9bad441262b68fd18
admin admin <evaladmin@example.com>
2020-03-27 05:46:01 +0000

    User admin wrote content /static-assets/js/useraction.js

--- static-assets/js/useraction.js
+++ static-assets/js/useraction.js
@@ -17,6 +17,11 @@ $(document).ready(function(){
     	});
     
     	$('div.code').click(function () {
+    	    var $temp = $("<input>");
+            $("body").append($temp);
+            $temp.val($(element).html()).select();
+            document.execCommand("copy");
+            $temp.remove();
  			alert("Code copied");
     	});
 });
