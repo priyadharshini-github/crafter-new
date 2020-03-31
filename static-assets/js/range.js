f45a4d5af77f1a6a8be43ac2e99b48cf2284ae36
admin admin <evaladmin@example.com>
2020-03-31 05:24:02 +0000

    User admin wrote content /static-assets/js/range.js

--- static-assets/js/range.js
+++ static-assets/js/range.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     function showProducts(minPrice, maxPrice) {
       $("#products tr").hide().filter(function() {
-        var price = parseInt($(this).data("percentage"), 10);
+        var price = $(this).data("percentage");
         console.log(price);
         return price >= minPrice && price <= maxPrice;
       }).show();
