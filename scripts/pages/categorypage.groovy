a3f38a9e9564b6e3a3108fce2c9f670a3640aba0
admin admin <evaladmin@example.com>
2020-03-31 13:18:27 +0000

    User admin wrote content /scripts/pages/categorypage.groovy

--- scripts/pages/categorypage.groovy
+++ scripts/pages/categorypage.groovy
@@ -1,1 +1,30 @@
-templateModel.categories = "Testing..."
+@Grab(group='org.json', module='json', version='20190722')
+
+import java.net.URI;
+import groovy.json.JsonSlurper;
+import org.json.XML;
+
+def authString = "Basic dXNlcjE6cGFzc3dvcmQx"
+
+// get Merchant store list
+def uriStoreList = new URI("http://vendor-api.eba-adup9t5c.us-east-2.elasticbeanstalk.com/api/vendor/rakuten/merchant/offers/true")
+org.apache.http.client.methods.HttpGet getStoreRequest= new org.apache.http.client.methods.HttpGet(uriStoreList);
+getStoreRequest.addHeader("Accept", "application/json");
+getStoreRequest.addHeader("Authorization", authString);
+org.apache.http.impl.client.DefaultHttpClient httpStoreClient = new org.apache.http.impl.client.DefaultHttpClient();
+org.apache.http.HttpResponse storeResponse = httpStoreClient.execute(getStoreRequest);
+
+status = storeResponse.getStatusLine().getStatusCode();
+def merchantOffers = ""
+
+if (status>= 300) {
+ throw new org.apache.http.client.ClientProtocolException("Unexpected error while fetching merchant offers - status: " + status)
+}
+org.apache.http.HttpEntity storeResponseEntity = storeResponse.getEntity();
+if (storeResponseEntity != null) {
+ merchantOffers = org.apache.http.util.EntityUtils.toString(storeResponseEntity);
+}
+
+templateModel.merchantOffers = merchantOffers;
+
+return merchantOffers
