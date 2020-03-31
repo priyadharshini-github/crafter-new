9eeffbe110356489c68bfb8f675a97219d8b01cb
admin admin <evaladmin@example.com>
2020-03-31 07:00:28 +0000

    User admin wrote content /static-assets/js/main.js

--- static-assets/js/main.js
+++ static-assets/js/main.js
@@ -5,8 +5,8 @@ function showtoast() {
 }
 
 $(document).ready(function(){
-  $("#myInput").on("keyup", function() {
-    var value = $(this).val().toLowerCase();
+  $("#mySubmit").on("click", function() {
+    var value = $("#myInput").val().toLowerCase();
     $("#myTable tr").filter(function() {
       $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
     });
