d73d2cad0cb521189a483a9fb53a424e99a5c151
admin admin <evaladmin@example.com>
2020-04-01 11:15:51 +0000

    User admin wrote content /static-assets/js/sort.js

--- static-assets/js/sort.js
+++ static-assets/js/sort.js
@@ -21,3 +21,21 @@
             
             _old.remove();
     	});
+    	
+    		$(document).ready(function() {
+				$('th').click(function(){
+					console.log('th clicked');
+					var table = $(this).parents('table').eq(0)
+					var rows = table.find('tr:gt(0)').toArray().sort(comparer($(this).index()))
+					this.asc = !this.asc
+					
+					for (var i = 0; i < rows.length; i++){table.append(rows[i])}
+				})
+				function comparer(index) {
+					return function(a, b) {
+						var valA = getCellValue(a, index), valB = getCellValue(b, index)
+						return $.isNumeric(valA) && $.isNumeric(valB) ? valA - valB : valA.toString().localeCompare(valB)
+					}
+				}
+				function getCellValue(row, index){ return $(row).children('td').eq(index).text() }
+			});
