848b9005e99f94bf28ae404697ce1e29547513e5
admin admin <evaladmin@example.com>
2020-04-04 11:46:58 +0000

    User admin wrote content /scripts/pages/template.groovy

--- scripts/pages/template.groovy
+++ scripts/pages/template.groovy
@@ -35,6 +35,26 @@ if (responseEntity != null) {
  categories = org.apache.http.util.EntityUtils.toString(responseEntity);
 }
 
+// get popular sites based on vendor rule engine
+def uriPop = new URI("http://vendor-api.eba-adup9t5c.us-east-2.elasticbeanstalk.com/api/vendor/user/popular/sites/rakuten")
+def authString = "Basic dXNlcjE6cGFzc3dvcmQx"
+org.apache.http.impl.client.DefaultHttpClient httpPopClient = new org.apache.http.impl.client.DefaultHttpClient();
+org.apache.http.client.methods.HttpGet getPopRequest= new org.apache.http.client.methods.HttpGet(uriPop);
+getRequest.addHeader("Accept", "application/json");
+getRequest.addHeader("Authorization", authString);
+org.apache.http.HttpResponse popResponse = httpPopClient.execute(getPopRequest);
+
+status = popResponse.getStatusLine().getStatusCode();
+def popularSites = ""
+
+if (status>= 300) {
+ throw new org.apache.http.client.ClientProtocolException("Unexpected error while fetching popular sites - status: " + status)
+}
+org.apache.http.HttpEntity popResponseEntity = popResponse.getEntity();
+if (popResponseEntity != null) {
+ popularSites = org.apache.http.util.EntityUtils.toString(popResponseEntity);
+}
+
 // get Merchant store list
 def uriStoreList = new URI("http://vendor-api.eba-adup9t5c.us-east-2.elasticbeanstalk.com/api/vendor/rakuten/merchant/offers/true")
 org.apache.http.client.methods.HttpGet getStoreRequest= new org.apache.http.client.methods.HttpGet(uriStoreList);
@@ -75,6 +95,7 @@ if (ruleResponseEntity != null) {
 
 //////////////////////
 templateModel.merchCategories = categories;
+templateModel.pouplarSites = popularSites;
 templateModel.merchantOffers = merchantOffers;
 templateModel.vendorRule = vendorRule;
 
