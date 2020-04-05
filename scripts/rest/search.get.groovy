a9354591b294850a15d56e0fed69a32ef0de7cab
admin admin <evaladmin@example.com>
2020-04-05 02:37:52 +0000

    User admin wrote content /scripts/rest/search.get.groovy

--- scripts/rest/search.get.groovy
+++ scripts/rest/search.get.groovy
@@ -1,49 +1,50 @@
 def merchCategories = {
     [
         {
-            merchant: {
-                mid: 8001,
-                merchantName: "Samsung"
-            },
-            couponOffers: [{
-                categories: [
-                    "Health & Beauty"
-                ],
-                promotionTypes: [
-                    "Dollar off"
-                ],
-                couponCode: "COUPON345",
-                offerDescription: "offer345",
-                offerStartDate: "2020-03-18",
-                offerEndDate: "2020-07-31",
-                clickUrl: "https://click.linksynergy.com/fs-bin/click?id=O9/jWAhJi3I&offerid=692069.5&type=3&subid=0",
-                impressionPixel: "https://ad.linksynergy.com/fs-bin/show?id=O9/jWAhJi3I&bids=692069.5&type=3&subid=0",
-                networkId: "1",
-                mid: "8001"
-            },
-            {
-                categories: [
-                    "Home Decor & Furniture"
-                ],
-                promotionTypes: [
-                    "Dollar off"
-                ],
-                couponCode: "COUPON678",
-                offerDescription: "offer678",
-                offerStartDate: "2020-03-18",
-                offerEndDate: "2020-08-31",
-                clickUr": "https://click.linksynergy.com/fs-bin/click?id=O9/jWAhJi3I&offerid=692069.7&type=3&subid=0",
-                impressionPixel: "https://ad.linksynergy.com/fs-bin/show?id=O9/jWAhJi3I&bids=692069.7&type=3&subid=0",
-                networkId: "1",
-                mid: "7005"
-            }],
-            cbOffer: {
-                mid: "8001",
-                commissionTerms: "7",
-                offerId: "765464",
-                offerName: "Macy's Welcome Program",
-                categories: "6 7 8"
-            }        
+            "merchant": {
+                "mid": "8001",
+                "merchantName": "Samsung"
+            },    
+            "couponOffers": [{
+                    "categories": [
+                        "Health & Beauty"
+                    ],
+                    "promotionTypes": [
+                        "Dollar off"
+                    ],
+                    "couponCode": "COUPON345",
+                    "offerDescription": "offer345",
+                    "offerStartDate": "2020-03-18",
+                    "offerEndDate": "2020-07-31",
+                    "clickUrl": "https://click.linksynergy.com/fs-bin/click?id=O9/jWAhJi3I&offerid=692069.5&type=3&subid=0",
+                    "impressionPixel": "https://ad.linksynergy.com/fs-bin/show?id=O9/jWAhJi3I&bids=692069.5&type=3&subid=0",
+                    "networkId": "1",
+                    "mid": "7005"
+                },
+                {
+                    "categories": [
+                        "Home Decor & Furniture"
+                    ],
+                    "promotionTypes": [
+                        "Dollar off"
+                    ],
+                    "couponCode": "COUPON2",
+                    "offerDescription": "offer2",
+                    "offerStartDate": "2020-03-18",
+                    "offerEndDate": "2020-08-31",
+                    "clickUrl": "https://click.linksynergy.com/fs-bin/click?id=O9/jWAhJi3I&offerid=692069.7&type=3&subid=0",
+                    "impressionPixel": "https://ad.linksynergy.com/fs-bin/show?id=O9/jWAhJi3I&bids=692069.7&type=3&subid=0",
+                    "networkId": "1",
+                    "mid": "7005"
+                }
+            ],
+            "cbOffer": {
+                "mid": "7005",
+                "commissionTerms": "7",
+                "offerId": "765464",
+                "offerName": "Macy's Welcome Program",
+                "categories": "6 7 8"
+            }
         }
     ]
 }
