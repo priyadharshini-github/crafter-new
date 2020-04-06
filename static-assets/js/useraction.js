6da8bddd522f22a69b3e20941c074afcafbfb96a
admin admin <evaladmin@example.com>
2020-04-06 06:05:22 +0000

    User admin wrote content /static-assets/js/useraction.js

--- static-assets/js/useraction.js
+++ static-assets/js/useraction.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     alert($("#hdMid").val());
     var moffers = JSON.parse($("#hdCpOffers").val());
-    console.log(moffers[0].merchant.mid);
+    console.log(moffers.clickUrl);
     $.ajax({
 		"url": "http://vendor-api.eba-adup9t5c.us-east-2.elasticbeanstalk.com/api/vendor/user/action/201",
           "method": "POST",
